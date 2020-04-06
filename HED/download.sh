c19a5fb3f803a014ace5d0e4fd2bf4d2ebbdc933
Satish Kumar Anbalagan <46766786+SatishKumarAnbalagan@users.noreply.github.com>
2020-04-06 01:15:34 -0400

    Update download.sh

--- HED/download.sh
+++ HED/download.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-hed/network-bsds500.pytorch -P $1
+wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=$2&password=$3&submit=Login' https://www.cityscapes-dataset.com/login/
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1 -P $1
