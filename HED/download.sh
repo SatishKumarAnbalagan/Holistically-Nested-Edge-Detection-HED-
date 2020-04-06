a0c8c6eeb4ae27a6f638607db763633216b9e182
Satish Kumar Anbalagan <46766786+SatishKumarAnbalagan@users.noreply.github.com>
2020-04-06 05:51:41 -0400

    Update download.sh

--- HED/download.sh
+++ HED/download.sh
@@ -5,6 +5,7 @@ pip install gitpython
 pip install python-resize-image
 pip install appdirs
 rm -r /content/Repo/
+git clone -v https://github.com/SatishKumarAnbalagan/EECE7398_DeepLearningInEmbeddedSystems.git /content/Repo/
 
 wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-hed/network-bsds500.pytorch -P $1
 
