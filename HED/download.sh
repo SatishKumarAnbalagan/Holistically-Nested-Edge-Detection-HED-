dc5ad809097cc13228eccfacf9541ca93e4dece5
Satish Kumar Anbalagan <46766786+SatishKumarAnbalagan@users.noreply.github.com>
2020-04-05 20:55:07 -0400

    added - download existing model and to run it

---
+++ HED/download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-hed/network-bsds500.pytorch -P ./gdrive/My\ Drive/ColabNotebooks/DLproject/pytorch-hed-master/
