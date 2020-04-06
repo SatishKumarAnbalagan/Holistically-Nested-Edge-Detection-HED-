92f161a4f7fe814ea8b92b4dd6c6f0e0a64c9c8c
Satish Kumar Anbalagan <46766786+SatishKumarAnbalagan@users.noreply.github.com>
2020-04-05 21:00:15 -0400

    Update download.sh

--- HED/download.sh
+++ HED/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-hed/network-bsds500.pytorch -P ./gdrive/My\ Drive/ColabNotebooks/DLproject/pytorch-hed-master/
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-hed/network-bsds500.pytorch -P $1
